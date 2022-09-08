0cd00d917dbe2862d596d4bd24157afe80c42366
Leth4L <carribean_08@mail.ru>
2022-09-08 15:24:55 +0300

    feat: add logger middleware

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+
 const router = require('./routes');
+
 const handlerErrors = require('./middlewares/handlerErrors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -19,7 +22,11 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
   useNewUrlParser: true,
 });
 
+app.use(requestLogger);
+
 app.use(router);
+
+app.use(errorLogger);
 
 app.use(errors());
 
