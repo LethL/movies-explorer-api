f3bd6962767a7d2e2aa2cc2fc09f0cb406e5b9d2
Leth4L <carribean_08@mail.ru>
2022-09-20 16:18:38 +0300

    feat: add CORS

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ const router = require('./routes');
 const handlerErrors = require('./middlewares/handlerErrors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
+const allowedCors = [
+  'https://dmitry.diplom.nomoredomains.sbs',
+  'http://dmitry.diplom.nomoredomains.sbs',
+  'https://api.dmitry.diplom.nomoredomains.sbs',
+  'http://api.dmitry.diplom.nomoredomains.sbs',
+  'https://localhost:3000',
+  'http://localhost:3000',
+];
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
@@ -17,6 +26,25 @@ app.use(cookieParser());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+
+    return res.status(200).send();
+  }
+
+  return next();
+});
 
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
   useNewUrlParser: true,
